1df400ae963a33b9fc4be328cc68be0039ff220e
Derick Del Rosario <derickmitchel@icloud.com>
2019-10-11 13:59:38 -0700

    update package.json with new scripts

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './a'
 console.log('hellowurld~!')
+
+
+// website and documentation https://webpack.js.org/
+// loaders are used to support other files, webpacker targets .js files by default
+// can find loaders on webpacker site https://webpack.js.org/concepts/loaders/
