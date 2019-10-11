76380947eaeddf9484bdf5d8c5926e3344597221
Derick Del Rosario <derickmitchel@icloud.com>
2019-10-11 14:00:08 -0700

    fixed typos

--- dist/main.js
+++ dist/main.js
@@ -127,6 +127,11 @@ __webpack_require__.r(__webpack_exports__);
 
 console.log('hellowurld~!')
 
+
+// website and documentation https://webpack.js.org/
+// loaders are used to support other files, webpacker targets .js files by default
+// can find loaders on webpacker site https://webpack.js.org/concepts/loaders/
+
 /***/ })
 
 /******/ });
